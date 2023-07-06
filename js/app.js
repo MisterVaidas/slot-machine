ba9b08f4285394b50068c9325a5ca1d64eb9251f
VAIDAS SIMKUS <VAIDAS.SIMKUS@YAHOO.COM>
2023-07-06 09:27:39 +0100

    updated branch with an issue

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 
 let balance = 0; // initialize balance
 let lastWin = 0;
+let spinning = false;
+
+
 
 function Fruit(name, value, image) {
 
@@ -22,6 +25,45 @@ function Fruit(name, value, image) {
   const lemon = new Fruit('lemon', 1, '/images/lemon.png');
 
 const fruits = [seven, melon, plum, banana, bar, orange, horseshoe, strawberry, bell, lemon];
+
+// animations 
+
+
+const images = [
+  "/images/spin1.png",
+  "/images/spin2.png",
+  "/images/spin3.png",
+  "/images/spin4.png",
+  "/images/spin5.png",
+  "/images/spin6.png",
+  "/images/spin7.png",
+  "/images/spin8.png",
+  "/images/spin9.png",
+  "/images/spin10.png",
+  "/images/spin11.png",
+  "/images/spin12.png",
+  "/images/spin13.png",
+  "/images/spin14.png",
+  "/images/spin15.png",
+  "/images/spin16.png",
+  "/images/spin17.png",
+  "/images/spin18.png",
+  "/images/spin19.png",
+  "/images/spin20.png",
+  "/images/spin21.png",
+  "/images/spin22.png",
+  "/images/spin23.png",
+  "/images/spin24.png",
+  "/images/spin25.png",
+  "/images/spin26.png",
+  "/images/spin27.png",
+  "/images/spin28.png"
+];
+
+var x = 0; //these variable set the image in the array that is the starting point of the animation
+var y = 0;
+var z = 0;
+
 
 // Function to prompt user to enter deposit amount
 function enterDeposit() {
@@ -87,7 +129,7 @@ function enterDeposit() {
         displayMessage('You already placed a bet. Please reset a bet or spin the reels');
      }
 
-    }
+  }
 
   function updateBetDisplay() {
     document.getElementById('bet').textContent = currentBet;
@@ -139,34 +181,170 @@ function enterDeposit() {
 
 let spinInterval;
 
+function populateReel(reelIndex, reels, randomFruits) {
+    setTimeout(() => {
+        let slots = reels[reelIndex].querySelectorAll('.symbol');
+        slots.forEach((slot, index) => {
+            slot.querySelector('img').src = randomFruits[reelIndex][index];
+        });
+    }, 1000 * (reelIndex + 1));
+}
 
-  function spinReels() { // spin reels function
-    displayMessage('');
+let animate1 = function() {
+ // animation function for reel1
+ document.getElementById("img").src = images[x]; //displays the image at position [x]
+ x++; //increases [x] by 1
+ if (images.length == x) {
+   // checks if x == last image in the animation array
+   x = 0; // if it is then the animation starts again from position [0]
+ }
+ document.getElementById("img2").src = images[x]; //displays the image at position [x+1]
+ x++;
+ if (images.length == x) {
+   x = 0;
+ }
+ document.getElementById("img3").src = images[x]; //displays the image at position [x+2]
+ x++;
+ if (images.length == x) {
+   x = 0;
+ }
+};
+
+let animate2 = function() {
+// animation function for reel2
+document.getElementById("img4").src = images[y];
+y++;
+if (images.length == y) {
+  y = 0;
+}
+document.getElementById("img5").src = images[y];
+y++;
+if (images.length == y) {
+  y = 0;
+}
+document.getElementById("img6").src = images[y];
+y++;
+if (images.length == y) {
+  y = 0;
+}
+};
+
+let animate3 = function() {
+ // animation function for reel3
+ document.getElementById("img7").src = images[z];
+ z++;
+ if (images.length == z) {
+   z = 0;
+ }
+ document.getElementById("img8").src = images[z];
+ z++;
+ if (images.length == z) {
+   z = 0;
+ }
+ document.getElementById("img9").src = images[z];
+ z++;
+ if (images.length == z) {
+   z = 0;
+ }
+};
+
+let Animate1, Animate2, Animate3;
+let interval1, interval2, interval3;
+
+function spinReels() { // spin reels function
+  displayMessage('');
+
+  clearInterval(interval1);
+  clearInterval(interval2);
+  clearInterval(interval3);
+
+    interval1 = setInterval(animate1, 30);
+    interval2 = setInterval(animate2, 30);
+    interval3 = setInterval(animate3, 30);
+
+    //setTimeout(stopAnimation, 3000);
+  
+}
+  // Hide the animation
+  function stopAnimation1() {
+    clearInterval(interval1);
+    document.getElementById("img").style.display = "none";
+    document.getElementById("img2").style.display = "none";
+    document.getElementById("img3").style.display = "none";
+
+    let randomFruits = populateReel();
+
+    document.getElementById("reel").src = randomFruits;
+    document.getElementById("reel").style.display = "block";
+
+    spinning = false;
+}
+
+  function stopAnimation2() {
+    clearInterval(interval2);
+    document.getElementById("img4").style.display = "none";
+    document.getElementById("img5").style.display = "none";
+    document.getElementById("img6").style.display = "none";
+
+    let randomFruits = populateReel();
+
+    document.getElementById("reel").src = randomFruits;
+    document.getElementById("reel").style.display = "block";
+
+    spinning = false;
+  }
+
+  function stopAnimation3() {
+    clearInterval(interval3);
+    document.getElementById("img7").style.display = "none";
+    document.getElementById("img8").style.display = "none";
+    document.getElementById("img9").style.display = "none";
+
+    let randomFruits = populateReel();
+
+    document.getElementById("reel").src = randomFruits;
+    document.getElementById("reel").style.display = "block";
+
+    spinning = false;
+
+  }
+  // Show the symbols
+  /*let slots = document.querySelectorAll('.symbol');
+  slots.forEach(slot => slot.style.display = "block");
+}*/
+
+function spin() {
+  
+
+  if (spinning) {
+    return;
+  }
+  spinning = true;
+  // Start animations
+  spinReels();
+
+    setTimeout(stopAnimation1, 1000); // Stop Animate1 after 3 seconds
+    setTimeout(stopAnimation2, 2000); // Stop Animate2 after 3.5 seconds
+    setTimeout(stopAnimation3, 3000); // Stop Animate3 after 4 seconds
+    setTimeout(function() { spinning = false; }, 3100);
+
+//stopAnimation();
+//setTimeout(stopAnimation, 3000);
+}
+
+
 
     if (currentBet > 0 && balance >= currentBet) {
         balance -= currentBet; // deduct bet from the balance
         updateBalanceDisplay();
         madeSpin = true;
 
-    // Set up spinning message
+        // Set up spinning message
         let dots = '';
         spinInterval = setInterval(() => {
-        dots = dots.length < 3 ? dots + '.' : '';
-        displayMessage('Spinning' + dots);
-    }, 500); // Interval time can be modified to control speed of animation
-
-     // spin the reels with delay
-     setTimeout(() => spinReel(0), 1000);
-     setTimeout(() => spinReel(1), 2000);
-     setTimeout(() => {
-         spinReel(2);
-         setTimeout(checkForWin, 2000); // Check for win after all reels have stopped spinning
-     }, 3000);
-            //balance -= currentBet; // deduct bet from the balance
-           // updateBalanceDisplay();
-           // madeSpin = true;
-            canPlaceBet = true; // allow bet changes
-        
+            dots = dots.length < 3 ? dots + '.' : '';
+            displayMessage('Spinning' + dots);
+        }, 500); 
 
         let reels = document.querySelectorAll('.reel');
         let randomFruits = [];
@@ -180,37 +358,21 @@ let spinInterval;
             });
         });
 
-        setTimeout(() => {
-            let slots = reels[0].querySelectorAll('.symbol');
-            slots.forEach((slot, index) => {
-                slot.querySelector('img').src = randomFruits[0][index];
-            });
-        }, 1000);
+        for (let i = 0; i < reels.length; i++) {
+            populateReel(i, reels, randomFruits);
+        }
 
-        setTimeout(() => {
-            let slots = reels[1].querySelectorAll('.symbol');
-            slots.forEach((slot, index) => {
-                slot.querySelector('img').src = randomFruits[1][index];
-            });
-        }, 2000);
-
-        setTimeout(() => {
-            let slots = reels[2].querySelectorAll('.symbol');
-            slots.forEach((slot, index) => {
-                slot.querySelector('img').src = randomFruits[2][index];
-            });
-        }, 3000);
+        canPlaceBet = true; // allow bet changes
 
     } else if (currentBet === 0) {
         displayMessage('You must place a bet first.');
-    
-
     } else {
         displayMessage('You dont have enough credits. Please add more!');
     }
 
     setTimeout(checkForWin, 3500);
-}
+
+
 
 function addCredits() {
     document.getElementById('addCreditsModal').style.display = 'block';
@@ -243,4 +405,3 @@ document.getElementById('addCreditsError').textContent = '';
 document.getElementById('addCreditsModal').style.display = 'none';
 
 });
-  
