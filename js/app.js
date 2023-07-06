ad58ffe8631909dcbe68d3f4f02c631b3786ecfc
VAIDAS SIMKUS <VAIDAS.SIMKUS@YAHOO.COM>
2023-07-06 09:59:15 +0100

    updated branch

--- js/app.js
+++ js/app.js
@@ -319,6 +319,19 @@ function spin() {
   if (spinning) {
     return;
   }
+
+  document.getElementById("img").style.display = "block";
+  document.getElementById("img2").style.display = "block";
+  document.getElementById("img3").style.display = "block";
+
+  document.getElementById("img4").style.display = "block";
+  document.getElementById("img5").style.display = "block";
+  document.getElementById("img6").style.display = "block";
+
+  document.getElementById("img7").style.display = "block";
+  document.getElementById("img8").style.display = "block";
+  document.getElementById("img9").style.display = "block";
+
   spinning = true;
   // Start animations
   spinReels();
